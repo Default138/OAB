8614ebace2aabc17fc8ce8d0aa889557ed924e14
Rafael Camargo <rc0102725@gmail.com>
2025-09-30 11:20:21 -0300

    Add files via upload

---
+++ Pessoas/modelo/Pessoa.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("PessoaFisica.php");
+require_once("PessoaJuridica.php");
+
+class Pessoa
+{
+
+    protected string $nome;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+};
